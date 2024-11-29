b0c2c21d152384e0bcf33091c7bc88116b2fb335
jayroguerrs <jguerreros@pucp.edu.pe>
2024-11-29 16:35:54 -0500

    .

---
+++ tools/gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const cleanCSS = require('gulp-clean-css');
+var terser = require('gulp-terser');
+var { parallel } = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+
+gulp.task('css', () => {
+    return gulp.src('../dev/**/*.css')
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(gulp.dest('../prod/'));
+  });
+
+gulp.task('img', function () {
+    return gulp.src('../dev/**/*.+(png|jpg|gif|svg)')
+        .pipe(imagemin())
+        .pipe(gulp.dest('../prod/'));
+});
+
+gulp.task('js', function () {
+    return gulp.src('../dev/**/*.js')
+        .pipe(terser())
+        .pipe(gulp.dest('../prod/'));
+});
+
+gulp.task('html', () => {
+    return gulp.src('../dev/**/*.php')
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(gulp.dest('../prod/'));
+  });
+
+gulp.task('default', parallel('css', 'img', 'js'));
+
+gulp.task('watch', function () {    
+    gulp.watch('../dev/admin/assets/css/*.css', parallel('css'));
+    gulp.watch('../dev/admin/assets/img/*.+(png|jpg|gif|svg)', parallel('img'));
+    gulp.watch('../dev/admin/assets/js/*.js', parallel('js'));
+});
